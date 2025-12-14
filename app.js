fe025f87f6e9a8aec7e5acd6cc2a359ddd815399
Abhijeet Raj <vermavijit16144@gmail.com>
2025-12-15 02:40:53 +0530

    Enable CORS for frontend access

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const { getStoredItems, storeItems } = require("./data/items");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
